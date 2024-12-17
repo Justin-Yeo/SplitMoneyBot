bbcc0764a3de25a6ec74ad011ce2a28d751e8c85
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-12-18 01:34:49 +0800

    Refactor and delete redudant commands

--- commands/start.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-start_command = "start"
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Welcome to your Splitwise Bot!")
